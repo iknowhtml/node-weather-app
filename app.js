26590750d3aa8395d91445f4f81f867995b9d26c
Aki Gao <xag9bb@virginia.edu>
2017-05-28 21:09:05 -0400

    complete 29. pretty printing objects

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const request = require('request');
 
-request({
-  url:'http://maps.googleapis.com/maps/api/geocode/json?address=13107%20meadow%20hall%20court%20virginia',
-  json: true,
-}, (error, response, body) => {
-  console.log(body);
-});
+request(
+  {
+    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=13107%20meadow%20hall%20court%20virginia',
+    json: true,
+  },
+  (error, response, body) => {
+    console.log(JSON.stringify(body, undefined, 2));
+  }
+);
