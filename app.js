0fc16bc1877c585d6764dc6e5833b7690995b6a0
Aki Gao <xag9bb@virginia.edu>
2017-05-28 21:58:15 -0400

    complete 33. abstracting callbacks

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode');
 const argv = yargs
   .options({
     a: {
@@ -13,23 +13,10 @@ const argv = yargs
   .help()
   .alias('help', 'h').argv;
 
-const encodedAddress = encodeURI(argv.address);
-request(
-  {
-    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true,
-  },
-  (error, response, body) => {
-    if (error) {
-      console.log('Unable to connect to Google servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-      console.log('unable to find that address.');
-    } else if (body.status === 'OK') {
-      const result = body.results[0];
-      const { lat, lng } = result.geometry.location;
-      console.log(`Address: ${result.formatted_address}`);
-      console.log(`Latitude: ${lat}`);
-      console.log(`Latitude: ${lng}`);
-    }
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if(errorMessage){
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
   }
-);
+});
