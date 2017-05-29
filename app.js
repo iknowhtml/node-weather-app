193d9863914da90050d73ed98fecd0500adc1819
Aki Gao <xag9bb@virginia.edu>
2017-05-29 18:12:14 -0400

    complete 34. wiring up weather search

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode');
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true,
+//     },
+//   })
+//   .help()
+//   .alias('help', 'h').argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 
-const geocode = require('./geocode/geocode');
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true,
-    },
-  })
-  .help()
-  .alias('help', 'h').argv;
+const request = require('request');
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if(errorMessage){
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
+request(
+  {
+    url: 'https://api.darksky.net/forecast/6ae8b6f925a4371aff96369fa46ec1e4/37.8267,-122.4233',
+    json: true,
+  },
+  (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      console.log(body.currently.temperature);
+    } else {
+      console.log('Unable to fetch weather.');
+    }
   }
-});
+);
